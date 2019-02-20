1b8b14973e5bee6022d761d63602110fe141029e
svadivazhagu <svadivazhagu@wpi.edu>
2019-02-20 13:48:16 -0500

    working on the chloropleth, got TOPOJson to work

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 # Data cleaning for a4-remix
 
 
@@ -12,9 +11,9 @@ water_global= water_global.set_index('Country')
 
 water_counties['id'] = water_counties['FIPS']
 water_counties['county'] = water_counties['COUNTY']
-water_counties_filtered = water_counties.set_index('id')[['STATE', 'TP-TotPop','TO-WGWTo', 'county']]
+water_counties_filtered = water_counties.set_index('id')[['STATE', 'TP-TotPop','DO-PSDel', 'county']]
 
-water_counties_filtered['groundwater_per_day'] = water_counties_filtered['TO-WGWTo'] * 1000000
+water_counties_filtered['groundwater_per_day'] = water_counties_filtered['DO-PSDel'] * 1000000
 water_counties_filtered['total_pop'] = water_counties_filtered['TP-TotPop'] * 1000
 
 water_counties_filtered['gal_per_person'] = water_counties_filtered['groundwater_per_day'] / water_counties_filtered['total_pop']
@@ -22,8 +21,8 @@ water_counties_filtered['state'] = water_counties_filtered['STATE']
 
 water_counties_final = water_counties_filtered[['state', 'total_pop', 'groundwater_per_day', 'gal_per_person', 'county']]
 print(water_counties.head())
-print(water_counties_final.sort_values(by='total_pop', ascending=False).head())
-
+#print(water_counties_final.sort_values(by='gal_per_person', ascending=False).head())
+print(water_counties_final[water_counties_final['state'] == 'VA'].sort_values(by='gal_per_person').head())
 water_counties_final.to_csv('data/water_counties_cleaned.csv')
 
 

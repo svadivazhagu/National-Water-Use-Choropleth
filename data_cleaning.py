6847e9a55237b5b68f35ca6fe9f78dd4576489f6
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2019-02-20 03:08:42 -0500

    working on choropleth map

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from matplotlib import pyplot as plt
 # Data cleaning for a4-remix
 
 
@@ -9,18 +9,24 @@ water_counties = pd.read_csv('data/water_counties.csv')
 
 water_global= water_global.set_index('Country')
 
-water_counties_filtered = water_counties.set_index('COUNTY')[['STATE', 'TP-TotPop','TO-WGWTo']]
+
+water_counties['id'] = water_counties['FIPS']
+water_counties['county'] = water_counties['COUNTY']
+water_counties_filtered = water_counties.set_index('id')[['STATE', 'TP-TotPop','TO-WGWTo', 'county']]
 
 water_counties_filtered['groundwater_per_day'] = water_counties_filtered['TO-WGWTo'] * 1000000
 water_counties_filtered['total_pop'] = water_counties_filtered['TP-TotPop'] * 1000
 
 water_counties_filtered['gal_per_person'] = water_counties_filtered['groundwater_per_day'] / water_counties_filtered['total_pop']
 water_counties_filtered['state'] = water_counties_filtered['STATE']
-water_counties_final = water_counties_filtered[['state', 'total_pop', 'groundwater_per_day', 'gal_per_person']]
-print(water_counties.head())
-print(water_counties_final.head())
 
-water_counties_final.to_csv('data/water_counties_cleaned')
+water_counties_final = water_counties_filtered[['state', 'total_pop', 'groundwater_per_day', 'gal_per_person', 'county']]
+print(water_counties.head())
+print(water_counties_final.sort_values(by='total_pop', ascending=False).head())
+
+water_counties_final.to_csv('data/water_counties_cleaned.csv')
+
+
 
 #calculation for
 

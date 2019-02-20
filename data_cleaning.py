41c4b360f6b8d28cb13a2fd86c1e95b495932595
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2019-02-20 02:07:11 -0500

    Decided to do a chloropleth map of water usage by USA counties due to better quality of data. Fully cleaned data and created csv with required data + new stat determining how much water a person uses in a day..problem is that in cases of low-populous counties this value is really high for some reason.....need to figure that out could provide good insights

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,11 +9,18 @@ water_counties = pd.read_csv('data/water_counties.csv')
 
 water_global= water_global.set_index('Country')
 
+water_counties_filtered = water_counties.set_index('COUNTY')[['STATE', 'TP-TotPop','TO-WGWTo']]
 
+water_counties_filtered['groundwater_per_day'] = water_counties_filtered['TO-WGWTo'] * 1000000
+water_counties_filtered['total_pop'] = water_counties_filtered['TP-TotPop'] * 1000
 
+water_counties_filtered['gal_per_person'] = water_counties_filtered['groundwater_per_day'] / water_counties_filtered['total_pop']
+water_counties_filtered['state'] = water_counties_filtered['STATE']
+water_counties_final = water_counties_filtered[['state', 'total_pop', 'groundwater_per_day', 'gal_per_person']]
+print(water_counties.head())
+print(water_counties_final.head())
 
+water_counties_final.to_csv('data/water_counties_cleaned')
 
-
-print(water_counties.to_string())
-
+#calculation for
 

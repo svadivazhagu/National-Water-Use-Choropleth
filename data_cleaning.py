c1d7073a817e5a5b08474685b430fb0a180d89b7
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2019-02-20 19:04:16 -0500

    made dataset cleaner, changed attr name to 'per-capita' to be more PC

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from decimal import *
+getcontext().prec = 2
 # Data cleaning for a4-remix
 
 
@@ -13,19 +15,31 @@ water_counties['id'] = water_counties['FIPS']
 water_counties['county'] = water_counties['COUNTY']
 water_counties_filtered = water_counties.set_index('id')[['STATE', 'TP-TotPop','DO-PSDel', 'county']]
 
-water_counties_filtered['groundwater_per_day'] = water_counties_filtered['DO-PSDel'] * 1000000
+water_counties_filtered['groundwater_per_day'] = (water_counties_filtered['DO-PSDel'] * 1000000).astype(int)
 water_counties_filtered['total_pop'] = water_counties_filtered['TP-TotPop'] * 1000
 
-water_counties_filtered['gal_per_person'] = water_counties_filtered['groundwater_per_day'] / water_counties_filtered['total_pop']
+water_counties_filtered['gal_per_capita'] = round((water_counties_filtered['groundwater_per_day'] / water_counties_filtered['total_pop']),2)
 water_counties_filtered['state'] = water_counties_filtered['STATE']
 
-water_counties_final = water_counties_filtered[['state', 'total_pop', 'groundwater_per_day', 'gal_per_person', 'county']]
-print(water_counties.head())
+water_counties_filtered['total_pop'] = (water_counties_filtered['TP-TotPop'] * 1000).astype(int)
+
+
+
+
+
+water_counties_final = water_counties_filtered[['state', 'total_pop', 'groundwater_per_day', 'gal_per_capita', 'county']]
 #print(water_counties_final.sort_values(by='gal_per_person', ascending=False).head())
-print(water_counties_final[water_counties_final['state'] == 'VA'].sort_values(by='gal_per_person').head())
+
+
 water_counties_final.to_csv('data/water_counties_cleaned.csv')
 
+quantiles = []
+for i in np.arange(0, 1.1, 0.1):
+    quantiles.append(int(water_counties_final['total_pop'].quantile(i)))
+print(quantiles)
 
-
+domain = [89, 5195, 9128, 13797, 19092, 25963, 37055, 52434, 90837, 203336, 10170292]
+domain.reverse()
+print(domain)
 #calculation for
 
